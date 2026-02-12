240806a0442dc2c334e848e5d196882f3302a7c8
Varun Shukla <varun.shukla1309@gmail.com>
2026-02-12 22:32:15 +0530

    Added task page

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,19 +1,69 @@
-import { useAuth } from "../auth/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Spinner, Alert, Table } from "react-bootstrap";
+import { getTasks } from "../services/task.service";
+import { type Task } from "../types/task";
 
-export default function Tasks() {
-  const { logout } = useAuth();
-  const navigate = useNavigate();
+const Tasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
+  const fetchTasks = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const data = await getTasks();
+      setTasks(data);
+    } catch (err: any) {
+      setError("Failed to load tasks.");
+    } finally {
+      setLoading(false);
+    }
   };
 
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  if (loading) {
+    return <Spinner animation="border" />;
+  }
+
+  if (error) {
+    return <Alert variant="danger">{error}</Alert>;
+  }
+
+  if (!loading && tasks.length === 0) {
+    return <Alert variant="info">No tasks found.</Alert>;
+  }
+
   return (
-    <div>
-      <h2>Tasks Page</h2>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Priority</th>
+          <th>DueDate</th>
+          <th>CreatedAt</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tasks.map((task) => (
+          <tr key={task.id}>
+            <td>{task.title}</td>
+            <td>{task.description}</td>
+            <td>{task.status}</td>
+            <td>{task.priority}</td>
+            <td>{task.dueDate}</td>
+            <td>{task.createdAt}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
-}
+};
+
+export default Tasks;
