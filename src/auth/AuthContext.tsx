212399c412c37ce8aca5808172d84fb66939e3ad
Varun Shukla <varun.shukla1309@gmail.com>
2026-02-16 18:18:27 +0530

    Adding jwt-decode logic

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -1,9 +1,17 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { type ReactNode } from "react";
+import { jwtDecode } from "jwt-decode";
 import { setAuthToken, setUnauthorizedHandler } from "../services/httpClient";
+
+type User = {
+  sub: string;
+  email: string;
+  role: string;
+};
 
 type AuthContextType = {
   isAuthenticated: boolean;
+  user: User | null;
   login: (token: string) => void;
   logout: () => void;
 };
@@ -12,18 +20,31 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
   const login = (token: string) => {
     setAuthToken(token);
+
+    const decoded: any = jwtDecode(token);
+
+    const mappedUser: User = {
+      sub: decoded.sub,
+      email: decoded.email,
+      role: decoded[
+        "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+      ],
+    };
+
+    setUser(mappedUser);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
     setAuthToken(null);
+    setUser(null);
     setIsAuthenticated(false);
   };
 
-  // 🔒 Register 401 handler once
   useEffect(() => {
     setUnauthorizedHandler(() => {
       logout();
@@ -31,7 +52,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
